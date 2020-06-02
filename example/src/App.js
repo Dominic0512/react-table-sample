37eea69533b4dd6994f5d6095b067101cbcdb5ea
Dominic Lee <dominiclee0512@gmail.com>
2020-06-02 18:33:18 +0800

    chore: setup basic table component lib structure with demo example.

---
+++ example/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { ExampleComponent } from 'table-component'
+import 'table-component/dist/index.css'
+const App = () => {
+  return <ExampleComponent text='Create React Library Example 😄' />
+}
+
+export default App
